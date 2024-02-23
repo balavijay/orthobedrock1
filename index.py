f9aa520955f0dee8a841234b7a48541c14d72df5
Balavijay Sundar Kamal <balavijay_sundar@persistent.com>
2024-02-23 21:01:30 +0530

    Code 6

--- index.py
+++ index.py
@@ -3,20 +3,20 @@ from llama_index.agent import ReActAgent
 
 
 llm = OpenAI(
-    model="davinci",
+    model="gpt-35-turbo-16k",
     api_key="sk-jfoNrqmyPiA_xop2fJ53Wg",
     api_base="https://4veynppxjm.us-east-1.awsapprunner.com",
     temperature=0.1
 )
 
 # Adjust the below parameters as per the model you've chosen
-# llm.__class__.metadata = LLMMetadata(
-#     context_window=4000, 
-#     num_output=1000,
-#     is_chat_model=True,
-#     is_function_calling_model=False, 
-#     model_name="bedrock/meta.llama2-13b-chat-v1",
-# )
+llm.__class__.metadata = LLMMetadata(
+    context_window=4000, 
+    num_output=1000,
+    is_chat_model=True,
+    is_function_calling_model=False, 
+    model_name="bedrock/meta.llama2-13b-chat-v1",
+)
 
 
 print(llm.chat([ChatMessage(role="user",content="Hi, who are you?")]).message.content)
