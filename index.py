a14ddc450af1305d58e8733a831beef445a2306a
Balavijay Sundar Kamal <balavijay_sundar@persistent.com>
2024-02-23 20:25:13 +0530

    Code 3

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ from llama_index.agent import ReActAgent
 
 
 llm = OpenAI(
-    model="./dataset.jsonl",
+    model="bedrock/meta.llama2-13b-chat-v1",
     api_key="sk-jfoNrqmyPiA_xop2fJ53Wg",
     api_base="https://4veynppxjm.us-east-1.awsapprunner.com",
     temperature=0.1
